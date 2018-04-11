17f6b599f5db81cf0b0d70f87b4f6c5fe70cb98f
xsc19850920 <xsc19850920@163.com>
2018-04-11 15:46:48 +0800

    commit for ribbon

---
+++ movie_ribbon_v1/src/main/java/com/genpact/cloud/controller/MovieRibbonController.java
@@ -0,0 +1,23 @@
+package com.genpact.cloud.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class MovieRibbonController {
+//	@Autowired
+//	private EurekaClient eurekaClient;
+//	@Autowired
+//	private DiscoveryClient discoveryClient;
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	@GetMapping("movie/user/{id}")
+	public String findById(@PathVariable("id") String id) {
+		String result = restTemplate.getForObject("http://USER-V1/user/"+id, String.class);
+		return result + " from  Movie"; 
+	}
+}
